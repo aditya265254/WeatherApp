d158cf96ff41e7cf673bf13638b5bb40c7c0b773
Aditya <akash.aditya001@gmail.com>
2026-05-05 23:23:06 +0530

    responsive

--- src/App.jsx
+++ src/App.jsx
@@ -7,31 +7,30 @@ import Weatherdetail from "./components/Weatherdetail";
 const App = () => {
   let [city, setCity] = useState("");
   let [weather, setWeather] = useState(null);
-  let [error, setError] = useState('')
+  let [error, setError] = useState("");
   const fetchWeather = async () => {
     try {
       const res = await fetch(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_WEATHER_API_KEY}&units=metric`,
       );
-      const data = await res.json() 
+      const data = await res.json();
       if (data.cod !== 200) {
-        setError('City not found!')
-        setWeather(null)
+        setError("City not found!");
+        setWeather(null);
       } else {
-        setError('')
-        setWeather(data)
+        setError("");
+        setWeather(data);
       }
     } catch (error) {
       console.log("error", error);
     }
-  } 
-
+  };
 
   return (
-    <div className="">
+    <div className="md:max-w-7xl md:mx-auto md:w-2/6 border md:mt-25 rounded-2xl overflow-auto  border-zinc-200 ">
       <Nav />
       <Searchbar city={city} setCity={setCity} fetchWeather={fetchWeather} />
-      {error && <p className='text-red-500 text-center mt-2'>{error}</p>}
+      {error && <p className="text-red-500 text-center mt-2">{error}</p>}
       <Weathercard weather={weather} />
       <Weatherdetail weather={weather} />
     </div>
